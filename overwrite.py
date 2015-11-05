90b68a3b08ab78db5909503dea9dea375831c3aa
EC2 Default User <ec2-user@ip-172-31-9-145.ap-northeast-1.compute.internal>
2015-11-05 04:02:08 +0000

    add overwrite.py

---
+++ overwrite.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys, time
+
+def overwrite(sen):
+  sys.stdout.write("\r" + sen)
+  sys.stdout.flush()
+
+def bar(a, b):
+  num = int(float(a)/b*100)
+  numSharp = num / 5
+  numSpace = 20 - numSharp
+  if num < 10:
+    return " " + str(num) + "%: " +  "[" + "#" * numSharp + " " * numSpace + "]"
+  else:
+    return str(num) + "%: " +  "[" + "#" * numSharp + " " * numSpace + "]"
