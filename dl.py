cd5df93e00d86e9dd59a221c5f72c0913d335da6
EC2 Default User <ec2-user@ip-172-31-9-145.ap-northeast-1.compute.internal>
2015-11-09 04:46:17 +0000

    function to download imagenet was implemented

--- dl.py
+++ dl.py
@@ -7,26 +7,31 @@ import subprocess
 import time
 import overwrite
 from datetime import datetime
+import Image
+import hashlib
+import threading
 
 #how to use imagenet api: http://image-net.org/download-API
 
 #base url
-#put files beneath this path
-base = "/mnt/s3/imagenet/categorize/"
+#put pictures beneath this path
+basePic = "/mnt/s3/imagenet/categorize/"
+#put urls beneath this path
+baseUrl = "/mnt/s3/imagenet/urls/"
 
 
 #download synset_list
 #download pictures of those ids
 if not "imagenet.synset.obtain_synset_list" in os.listdir("./"):
   print("downloading synset_list...")
-  cmd ="wget http://www.image-net.org/api/text/imagenet.synset.obtain_synset_list"
+  cmd ="wget http://www.image-net.org/api/text/imagenet.synset.obtain_synset_list -q"
   subprocess.call(cmd, shell=True)
 
 #download words.txt
 #the association of ids and tags
 if not "words.txt" in os.listdir("./"):
   print("downloading the table about ids and tags...")
-  cmd ="wget http://image-net.org/archive/words.txt"
+  cmd ="wget http://image-net.org/archive/words.txt -q"
   subprocess.call(cmd, shell=True)
 
 def storeToArray(name):
@@ -56,28 +61,122 @@ synset = storeToArray("./imagenet.synset.obtain_synset_list")
 #put image id at the end
 apiUrl = "http://www.image-net.org/api/text/imagenet.synset.geturls?wnid="
 
+files = [re.sub(r"\.txt$", "", name) for name in os.listdir(baseUrl) if re.findall(r"\.txt$",name)]
+setSynset = set(synset)
+fileUrl = list(setSynset.difference(set(files)))
+
 #get all urls
-if not "allUrls.json" in os.listdir("./"):
-  urls = {}
-  count = 0
-  numId = len(synset)
-  print("getting all urls....")
-  for picId in synset:
-    count = count + 1
-    sen = overwrite.bar(count, numId)
-    overwrite.overwrite(sen)
-    cmd = "wget " + apiUrl + picId + " -O tmp -q"
-    subprocess.call(cmd, shell=True)
-    urls[picId] = storeToArray("./tmp")
-  
-  #remove tmp file
+numId = len(synset)
+count = numId - len(fileUrl)
+print("getting all urls....")
+for picId in fileUrl:
+  count = count + 1
+  sen = overwrite.bar(count, numId)
+  sen = sen + " " + picId
+  overwrite.overwrite(sen)
+  cmd = "wget " + apiUrl + picId + " -O tmp -q"
   subprocess.call(cmd, shell=True)
-  cmd = "rm tmp"
+  cmd = "mv tmp " + baseUrl + picId + ".txt"
+  subprocess.call(cmd, shell=True)
+print("urls are all prepared!")
+
+def checkExistance(path):
+  if os.path.exists(path):
+    return True
+  else:
+    return False
+
+def makeDirectory(path):
+  if not checkExistance(path):
+    os.makedirs(path)
+
+def downloadPic(url, name):
+  cmd = "wget " + url + " -O " + name + " -q"
+  subprocess.call(cmd, shell=True)
+
+def extractExtension(name):
+  return re.findall(r"^.*(\..*)$", name)[0]
+
+def moveFile(path, name):
+  cmd = "mv " + name + " " + path
+  subprocess.call(cmd, shell=True)
+
+def writeToFile(arr, path):
+  f=open(path, "w")
+  for line in arr:
+    f.write(line + "\n")
+  f.close()
+
+
+class Command(object):
+    def __init__(self, cmd):
+        self.cmd = cmd
+        self.process = None
+
+    def run(self, timeout):
+        def target():
+            self.process = subprocess.Popen(self.cmd, shell=True)
+            self.process.communicate()
+        thread = threading.Thread(target=target)
+        thread.start()
+        thread.join(timeout)
+        if thread.is_alive():
+            self.process.terminate()
+            thread.join()
+        print self.process.returncode
+
+
+baseLog = "/mnt/s3/imagenet/log/"
+count = 0
+numPic = len(files)
+
+for f in files:
+  count = count + 1
+  fail = []
+  dirPath = basePic + f
+  makeDirectory(dirPath)
+  urls = storeToArray(baseUrl + f + ".txt")
+  sen = overwrite.bar(count, numPic)
+  sen = sen + " " + f
+  overwrite.overwrite(sen)
+  for url in urls:
+    extension = extractExtension(url)
+    picName = dirPath + "/" + hashlib.sha224(url).hexdigest() + extension
+    if not checkExistance(picName):
+      try:
+        if not len(extension):
+          raise ValueError('extension is blank')
+        cmd = "wget " + url + " -O ./tmp -q"
+        command = Command(cmd)
+        if command.run(timeout = 30):
+          raise ValueError("failed downloafing")
+        #verify whether it's not broken or not
+        tmp = Image.open("./tmp")
+        tmp.verify()
+        tmp.load_end()
+        #if picture is too small, raise error
+        if tmp.size[0] * tmp.size[1] <= 50 * 50:
+          raise ValueError('picture is too small')
+        moveFile(picName, "./tmp")
+      except:
+        fail.append(url)
+  #if some files were failed, take the log
+  if len(fail):
+    writeToFile(fail, baseLog + f + ".log")
   
-  #save file
-  with open("allUrls.json", 'w') as outfile:
-    json.dump(urls, outfile)
-else:
-  with open("allUrls.json", 'r') as json_file:
-    urls = json.load(json_file)
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
